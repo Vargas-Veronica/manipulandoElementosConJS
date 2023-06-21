189a57359d6866e782d994476abfe4d79dc72b22
Vargas-Veronica <vargas.veronica55@gmail.com>
2023-06-20 22:34:33 -0300

    manipulando elementos

--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -3,10 +3,10 @@ window.addEventListener("load", function() {
 const body = document.querySelector("body");
 const h1 = document.querySelector("h1");
 
-const oscuro = prompt("¿Desea modo oscuro?");
+let oscuro = prompt("¿Desea modo oscuro?");
 if (oscuro == "si" || oscuro == "SI") {
     body.style.backgroundColor = "#7f7f7f";
-    body.classList.add = "fondoMoviesList";
+    body.classList.add("fondoMoviesList");
 }
 
 h1.innerHTML = "LISTADO DE PELÍCULAS";
